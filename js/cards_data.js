d4e362f72d6cd19bc649b6df17f50659124900f1
TeodorMihalchan <125436669+TeodorMihalchan@users.noreply.github.com>
2023-05-10 09:49:52 +0300

    Add files via upload

---
+++ js/cards_data.js
@@ -0,0 +1,196 @@
+const cardsData = [
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Ноутбук Acer Aspire 7 A715-42G-R3EZ (NH.QBFEU.00C) Charcoal Black',
+      description: 'Екран 15.6" IPS (1920x1080) Full HD, матовий / AMD Ryzen 5 5500U (2.1 - 4.0 ГГц) / RAM 16 ГБ / SSD 512 ГБ / nVidia GeForce GTX 1650, 4 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / веб-камера / без ОС / 2.15 кг / чорний',
+      cost: '29999'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Ноутбук ASUS Laptop X515EA-BQ2066 (90NB0TY1-M00VF0) Slate Grey',
+      description: 'Екран 15.6" IPS (1920x1080) Full HD, матовий / Intel Core i3-1115G4 (3.0 - 4.1 ГГц) / RAM 12 ГБ / SSD 512 ГБ / Intel UHD Graphics / без ОД / Wi-Fi / Bluetooth / веб-камера / без ОС / 1.8 кг / сірий',
+      cost: '19999'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Ноутбук Lenovo IdeaPad 3 15ALC6 (82KU01C4RA) Arctic Grey',
+      description: 'Екран 15.6" IPS (1920x1080) Full HD, матовий / AMD Ryzen 5 5500U (2.1 - 4.0 ГГц) / RAM 16 ГБ / SSD 512 ГБ / AMD Radeon Graphics / без ОД / Wi-Fi / Bluetooth / веб-камера / без ОС / 1.65 кг / сірий',
+      cost: '23999'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Ноутбук Apple MacBook Air 13" M1 256GB 2020 (MGN63) Space Gray',
+      description: 'Екран 13.3" Retina (2560x1600) WQXGA, глянсовий / Apple M1 / RAM 8 ГБ / SSD 256 ГБ / Apple M1 Graphics / Wi-Fi / Bluetooth / macOS Big Sur / 1.29 кг / сірий',
+      cost: '43499'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Ноутбук ASUS TUF Gaming A15 FA506ICB-HN119 (90NR0667-M00KT0) Graphite Black',
+      description: 'Екран 15.6" IPS (1920x1080) Full HD 144 Гц, матовий / AMD Ryzen 5 4600H (3.0 - 4.0 ГГц) / RAM 16 ГБ / SSD 512 ГБ / nVidia GeForce RTX 3050, 4 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / веб-камера / без ОС / 2.3 кг / черный',
+      cost: '35999'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 6',
+      description: 'Опис картки 6',
+      cost: '100'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 7',
+      description: 'Опис картки 7',
+      cost: '1800'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 8',
+      description: 'Опис картки 8',
+      cost: '100000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 9',
+      description: 'Опис картки 9',
+      cost: '15000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 10',
+      description: 'Опис картки 10',
+      cost: '10000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 11',
+      description: 'Опис картки 11',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 12',
+      description: 'Опис картки 12',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 13',
+      description: 'Опис картки 13',
+      cost: '10050'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 14',
+      description: 'Опис картки 14',
+      cost: '15000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 15',
+      description: 'Опис картки 15',
+      cost: '140400'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 16',
+      description: 'Опис картки 16',
+      cost: '14000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 17',
+      description: 'Опис картки 17',
+      cost: '10800'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 18',
+      description: 'Опис картки 18',
+      cost: '18000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 19',
+      description: 'Опис картки 19',
+      cost: '12000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 20',
+      description: 'Опис картки 20',
+      cost: '21000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 21',
+      description: 'Опис картки 21',
+      cost: '61000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 22',
+      description: 'Опис картки 22',
+      cost: '13000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 23',
+      description: 'Опис картки 23',
+      cost: '13400'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 24',
+      description: 'Опис картки 24',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 25',
+      description: 'Опис картки 25',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 26',
+      description: 'Опис картки 26',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 27',
+      description: 'Опис картки 27',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 28',
+      description: 'Опис картки 28',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 29',
+      description: 'Опис картки 29',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 30',
+      description: 'Опис картки 30',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 31',
+      description: 'Опис картки 31',
+      cost: '1000'
+    },
+    {
+      imgSrc: 'images/img.jpg',
+      title: 'Картка 32',
+      description: 'Опис картки 32',
+      cost: '11000'
+    }
+  ];
+
+  let cartList = [];
---
+++ js/sub.js
@@ -0,0 +1,24 @@
+(function subscribeCheck() {
+  const isSubscribed = localStorage.getItem('isSubscribed');
+
+  if (!isSubscribed) {
+    setTimeout(() => {
+      const subs = document.getElementById("sub");
+      const overlayHtml = `<div style="position: fixed; bottom: 0; left: 0; width: 100%; display: flex; flex-direction: column; z-index: 9998; border-radius: 0;" id="subscribe-overlay"> <div style="background: rgb(100, 0, 17); padding: 20px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.5); text-align: center;"> <p style="margin: 0 0 20px;">Підпишіться на наші повідомлення:</p> <button style="margin: 10px; border-radius: 6px;" id="subscribe-accept-button">Підписатися</button> <button style="margin: 10px; border-radius: 6px;" id="subscribe-cancel-button">Не зараз</button> </div> </div>`;
+      subs.innerHTML = overlayHtml;
+
+      const acceptButton = document.querySelector('#subscribe-accept-button');
+      acceptButton.addEventListener('click', () => {
+        localStorage.setItem('isSubscribed', true);
+        const overlay = document.querySelector('#subscribe-overlay');
+        overlay.style.display = 'none';
+      });
+
+      const cancelButton = document.querySelector('#subscribe-cancel-button');
+      cancelButton.addEventListener('click', () => {
+        const overlay = document.querySelector('#subscribe-overlay');
+        overlay.style.display = 'none';
+      });
+    }, 4000);
+  }
+})(); // Функція обробника підписки.
